0bb64db825de56f794a075b382779b8a68c77d15
temporaryzzz <Mr.Nik009@bk.ru>
2025-07-03 23:43:45 +0300

    add: sing-in and sing-up + styles

--- taskora-frontend/src/App.tsx
+++ taskora-frontend/src/App.tsx
@@ -1,15 +1,20 @@
 //import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router';
-import './App.scss'
 import TestBuild from './components/test';
+import SingInForm from './components/singIn';
+import SingUpForm from './components/singup';
 
 function App() {
 
   return (
     <BrowserRouter>
         <Routes>
-            <Route path='' element={<div>TASKORA</div>} />
+            <Route path='' element={<SingInForm />} />
+            <Route path='sing-up' element={<SingUpForm />} />
             <Route path='test' element={<TestBuild />} />
+            <Route path='profile' element={<div>PROFILE</div>} />
+            <Route path='task-lists' element={<div>TASK-MANAGER</div>} />
+            <Route path='board' element={<div>ONLINE-BOARD</div>} />
         </Routes>
     </BrowserRouter>  
   )
---
+++ taskora-frontend/src/components/singup.tsx
@@ -0,0 +1,80 @@
+//import { useState } from 'react';
+import type { FormEvent } from 'react';
+import { useNavigate } from 'react-router';
+import '../styles.scss';
+
+
+function SingUpForm() {
+    const navigate = useNavigate()
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault()
+        navigate('../', {replace: false})//Для рендиринга этого компонента по url '/main'
+    };
+
+    return (
+        <>
+            <h1>TASKORA</h1>
+            <form id="singup-form" action='' data-js-singin 
+                onSubmit={handleSubmit} >
+                <p className="field">
+                    <label className="field__label" htmlFor="login">Username or email address</label>
+                    <input
+                    className="field__control" 
+                    id="login" 
+                    name="login" 
+                    minLength={2}
+                    maxLength={16}
+                    aria-errormessage="login-errors"
+                    data-login
+                    required/>
+
+                    <span className="field__errors" id="login-errors" data-js-singin-field-errors></span>
+                </p>
+                <p className="field">
+                    <label className="field__label" htmlFor="password">Password</label>
+                    <input 
+                    className="field__control" 
+                    id="password" 
+                    name="password" 
+                    type="password" 
+                    minLength={4}
+                    maxLength={16}
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,16}"
+                    title="Пароль должен содержать хотя бы 1 заглавную и 1 строчную букву, a также хотя бы 1 цифру."
+                    aria-errormessage="password-errors"
+                    data-password
+                    required/>
+
+                    <span className="field__errors" id="password-errors" data-js-singin-field-errors></span>
+                </p>
+
+                <p className="field">
+                    <label className="field__label" htmlFor="password">Repeat password</label>
+                    <input 
+                    className="field__control" 
+                    id="password" 
+                    name="password" 
+                    type="password" 
+                    minLength={4}
+                    maxLength={16}
+                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,16}"
+                    title="Пароль должен содержать хотя бы 1 заглавную и 1 строчную букву, a также хотя бы 1 цифру."
+                    aria-errormessage="password-errors"
+                    data-password
+                    required/>
+
+                    <span className="field__errors" id="password-errors" data-js-singin-field-errors></span>
+                </p>
+
+                <button className="main-button main-button--green" type="submit" >Sing-Up</button>
+            </form>
+
+            <form>
+                <button className="main-button main-button--back" onClick={() => navigate('../')} >Назад</button>
+            </form>        
+        </>
+    );
+}
+
+export default SingUpForm;
