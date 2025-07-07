7e82d59da38331b0eeca7920c8781d6a06b5b908
temporaryzzz <Mr.Nik009@bk.ru>
2025-07-07 16:18:54 +0300

    add: header-menu

--- taskora-frontend/src/App.tsx
+++ taskora-frontend/src/App.tsx
@@ -1,8 +1,8 @@
 //import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router';
-import TestBuild from './components/test';
 import SingInForm from './components/singIn';
 import SingUpForm from './components/singup';
+import Header from './components/header';
 
 function App() {
 
@@ -11,10 +11,21 @@ function App() {
         <Routes>
             <Route path='' element={<SingInForm />} />
             <Route path='sing-up' element={<SingUpForm />} />
-            <Route path='test' element={<TestBuild />} />
-            <Route path='profile' element={<div>PROFILE</div>} />
-            <Route path='task-lists' element={<div>TASK-MANAGER</div>} />
-            <Route path='board' element={<div>ONLINE-BOARD</div>} />
+            <Route path='profile' element={
+              <>
+                <Header active="profile"/>
+                <div>PROFILE</div>
+              </>} />
+            <Route path='task-lists' element={
+              <>
+                <Header active="task-lists"/>
+                <div>TASK-MANAGER</div>
+              </>} />
+            <Route path='task-board' element={
+              <>
+                <Header active="task-board"/>
+                <div>TASK-BOARD</div>
+              </>} />
         </Routes>
     </BrowserRouter>  
   )
---
+++ taskora-frontend/src/components/header.tsx
@@ -0,0 +1,51 @@
+import { useState, useRef, useEffect } from 'react';
+import { useNavigate } from 'react-router';
+import '../styles.scss';
+
+
+type HeaderProps = {
+  active: string;
+};
+
+function Header(props: HeaderProps) {
+    const [userName] = useState('Username')
+
+    const navigate = useNavigate()
+
+    const profileRef = useRef<HTMLButtonElement>(null);
+    const taskListsRef = useRef<HTMLButtonElement>(null);
+    const taskBoardRef = useRef<HTMLButtonElement>(null);
+
+    const setActiveElement = (active: string) => {
+        if (profileRef.current) profileRef.current.classList.remove("tabs__tab--active");
+        if (taskListsRef.current) taskListsRef.current.classList.remove("tabs__tab--active");
+        if (taskBoardRef.current) taskBoardRef.current.classList.remove("tabs__tab--active");
+
+        // Устанавливаем активный класс для нужной кнопки
+        switch (active) {
+            case "profile":
+                if (profileRef.current) profileRef.current.classList.add("tabs__tab--active");
+                break;
+            case "task-lists":
+                if (taskListsRef.current) taskListsRef.current.classList.add("tabs__tab--active");
+                break;
+            case "task-board":
+                if (taskBoardRef.current) taskBoardRef.current.classList.add("tabs__tab--active");
+                break;
+            default:
+                console.log(`Неизвестный активный элемент: ${active}`);
+        }
+    }
+
+    useEffect(() => {setActiveElement(props.active)}, [props.active])
+
+    return (
+        <nav className="tabs">
+            <button id="profile" ref={profileRef} className="tabs__tab" onClick={() => navigate('../profile', {replace: false})}>{userName}</button> 
+            <button id="task-lists" ref={taskListsRef} className="tabs__tab" onClick={() => navigate('../task-lists', {replace: false})}>Task-manager</button>
+            <button id="task-board" ref={taskBoardRef} className="tabs__tab" onClick={() => navigate('../task-board', {replace: false})}>Task-board</button>
+        </nav>
+    )
+}
+
+export default Header;
