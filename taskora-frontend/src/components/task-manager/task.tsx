f7c7ebd423795c8ac69de3e7a633e20cab2f49fd
temporaryzzz <Mr.Nik009@bk.ru>
2025-07-13 21:41:27 +0300

    add: task completed state and switch his state

--- taskora-frontend/src/components/task-manager/task.tsx
+++ taskora-frontend/src/components/task-manager/task.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import '../../styles.scss';
 
 type TaskInfo = {
@@ -9,11 +10,44 @@ type TaskInfo = {
 }
 
 function Task(task: TaskInfo) {
+    const stateClasses ={
+        defaultClass: "task-list__task",
+        completedClass: "task-list__task--completed"
+    }
 
+    const taskRef = useRef<HTMLDivElement>(null);
+    const taskCheckbox = useRef<HTMLInputElement>(null);
+    const [taskCompletedState, setTaskCompletedState] = useState(false)
+
+    const InizializateStateTask = () => {
+        if(task.completed === "true") {
+            console.log(taskRef.current)
+            if (taskRef.current) taskRef.current.classList.add(stateClasses.completedClass)
+            if (taskCheckbox.current) taskCheckbox.current.checked = true  
+        }
+    }
+
+    const setStateTask = () => { 
+        setTaskCompletedState(!taskCompletedState)
+
+        if (taskRef.current) {
+            if (taskRef.current.matches(stateClasses.completedClass)) {
+                taskRef.current.classList.remove(stateClasses.completedClass)
+            }
+            else {
+                taskRef.current.classList.add(stateClasses.completedClass)
+            }
+        }
+
+        if (taskCheckbox.current) taskCheckbox.current.checked = taskCompletedState  
+
+    }
+
+    useEffect(InizializateStateTask, [task.completed])
 
     return (
-        <div className='task-list__task'> 
-            <input type='checkbox' id='completed'></input>
+        <div className='task-list__task' ref={taskRef}> 
+            <input type='checkbox' id='completed' ref={taskCheckbox} onChange={setStateTask}></input>
             <h4>{task.title}</h4> 
             <p>{task.description}</p>
         </div>    
