317c281b8a850e89854e9ddc178a9cd2ddac53a7
Dmitry Grand <Russian.Gaimer@yandex.ru>
2025-08-16 21:36:02 +0300

    Initial commit

--- taskora-backend/src/main/java/com/taskora/backend/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.taskora.backend.repository;
-
-import com.taskora.backend.model.entity.Task;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TaskRepository extends CrudRepository<Task, Long> {
-}
