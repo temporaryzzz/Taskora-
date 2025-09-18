f84bab90b6183801e5149263d007c09924a284f8
temporaryzzz <Mr.Nik009@bk.ru>
2025-09-18 22:01:46 +0300

    recovery of lost files

---
+++ taskora-frontend/src/main.tsx
@@ -0,0 +1,8 @@
+//import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles.scss'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+    <App />
+)
