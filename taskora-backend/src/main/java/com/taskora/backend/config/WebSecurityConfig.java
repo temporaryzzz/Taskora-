b4e6e33e8755096e84a87d920f1edcb2f26c0fb1
Dmitry Grand <Russian.Gaimer@yandex.ru>
2025-11-28 14:39:14 +0300

    refactor: cors configuration for security

--- taskora-backend/src/main/java/com/taskora/backend/config/WebSecurityConfig.java
+++ taskora-backend/src/main/java/com/taskora/backend/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.taskora.backend.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +15,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.taskora.backend.security.AuthEntryPointJwt;
 import com.taskora.backend.security.AuthTokenFilter;
 import com.taskora.backend.service.CustomUserDetailsService;
-
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.Arrays;
 
 /**
  * Класс, отвечающий за цепочку фильтров проверки авторизации
@@ -64,8 +64,10 @@ public class WebSecurityConfig {
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/api/**", configuration);
+        
         return source;
     }
 
